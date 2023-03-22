9892e474cac0b96b10f4e0463bdacc73eca57b6d
Maritim Daniel <maritimdaniel07@gmai.com>
2023-03-22 16:48:59 +0300

    clean-up

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,3 @@
-console.log(window.innerWidth);
-console.log(document.documentElement.clientWidth);
-console.log(window.pageXOffset);
 if (window.innerWidth < 801) {
   const navigation = document.querySelector(".navigation");
   const menuBtn = document.querySelector(".hamburger");
